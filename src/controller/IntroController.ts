30d787fe6dbfd98f0505d685484db0a96bdb0698
funnysunny08 <rosa7536@naver.com>
2022-11-20 02:11:46 +0900

    [feature/#5] FEAT: x-자기소개서 생성

--- src/controller/IntroController.ts
+++ src/controller/IntroController.ts
@@ -8,15 +8,15 @@ import { IntroService } from "../service";
  * @access Public
  */
 const createIntro = async (req: Request, res: Response) => {
-    const introCreateDTO: IntroCreateDTO = req.body;
+  const introCreateDTO: IntroCreateDTO = req.body;
 
-    const data = await IntroService.createIntro(introCreateDTO);
+  const data = await IntroService.createIntro(introCreateDTO);
 
-    if(!data) {
-      return res.status(404).json({ status: 404, message: "x-소개서 생성 실패" });
-    }
-    return res. status(200).json({ status: 200, message: "x-소개서 생성 성공", data});
-}
+  if(!data) {
+    return res.status(404).json({ status: 404, message: "x-소개서 생성 실패" });
+  }
+  return res. status(200).json({ status: 200, message: "x-소개서 생성 성공", data});
+};
 
 
 const userController = {
